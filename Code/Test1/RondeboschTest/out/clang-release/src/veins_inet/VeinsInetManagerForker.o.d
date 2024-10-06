d0e9c3c9719198ddcdd12ef3396b37c6a1325bf1
Veins (password is veins) <veins@instant-veins.car2x.org>
2024-10-06 22:44:57 +0000

    Code for initial testing added

---
+++ Code/Test1/RondeboschTest/out/clang-release/src/veins_inet/VeinsInetManagerForker.o.d
@@ -0,0 +1,124 @@
+../out/clang-release/src/veins_inet/VeinsInetManagerForker.o: \
+  veins_inet/VeinsInetManagerForker.cc \
+  veins_inet/VeinsInetManagerForker.h veins_inet/veins_inet.h \
+  /home/veins/src/veins/src/veins/veins.h \
+  /home/veins/src/omnetpp-5.7/include/omnetpp.h \
+  /home/veins/src/inet/src/inet/common/INETDefs.h \
+  /home/veins/src/inet/src/inet/common/precompiled_release.h \
+  /home/veins/src/inet/src/inet/common/precompiled.h \
+  /home/veins/src/inet/src/inet/opp_defines.h \
+  /home/veins/src/inet/src/inet/features.h \
+  /home/veins/src/inet/src/inet/common/Compat.h \
+  /home/veins/src/inet/src/inet/common/InitStages.h \
+  /home/veins/src/veins/src/veins/modules/mobility/traci/TraCIScenarioManagerForker.h \
+  /home/veins/src/veins/src/veins/modules/mobility/traci/TraCIScenarioManager.h \
+  /home/veins/src/veins/src/veins/base/utils/Coord.h \
+  /home/veins/src/veins/src/veins/base/utils/FWMath.h \
+  /home/veins/src/veins/src/veins/base/modules/BaseWorldUtility.h \
+  /home/veins/src/veins/src/veins/base/connectionManager/BaseConnectionManager.h \
+  /home/veins/src/veins/src/veins/base/utils/AntennaPosition.h \
+  /home/veins/src/veins/src/veins/base/connectionManager/NicEntry.h \
+  /home/veins/src/veins/src/veins/base/utils/Heading.h \
+  /home/veins/src/veins/src/veins/modules/utility/HasLogProxy.h \
+  /home/veins/src/veins/src/veins/base/utils/FindModule.h \
+  /home/veins/src/veins/src/veins/modules/obstacle/ObstacleControl.h \
+  /home/veins/src/veins/src/veins/modules/obstacle/Obstacle.h \
+  /home/veins/src/veins/src/veins/modules/world/annotations/AnnotationManager.h \
+  /home/veins/src/veins/src/veins/modules/utility/BBoxLookup.h \
+  /home/veins/src/veins/src/veins/modules/obstacle/VehicleObstacleControl.h \
+  /home/veins/src/veins/src/veins/base/utils/Move.h \
+  /home/veins/src/veins/src/veins/modules/obstacle/MobileHostObstacle.h \
+  /home/veins/src/veins/src/veins/modules/mobility/traci/TraCIBuffer.h \
+  /home/veins/src/veins/src/veins/modules/mobility/traci/TraCIConstants.h \
+  /home/veins/src/veins/src/veins/modules/mobility/traci/TraCIColor.h \
+  /home/veins/src/veins/src/veins/modules/mobility/traci/TraCIConnection.h \
+  /home/veins/src/veins/src/veins/modules/mobility/traci/TraCICoord.h \
+  /home/veins/src/veins/src/veins/modules/mobility/traci/TraCICoordinateTransformation.h \
+  /home/veins/src/veins/src/veins/modules/mobility/traci/VehicleSignal.h \
+  /home/veins/src/veins/src/veins/base/utils/EnumBitset.h \
+  /home/veins/src/veins/src/veins/modules/mobility/traci/TraCIRegionOfInterest.h \
+  /home/veins/src/veins/src/veins/modules/mobility/traci/TraCILauncher.h \
+  veins_inet/VeinsInetManagerBase.h \
+  /home/veins/src/veins/src/veins/modules/utility/SignalManager.h
+
+veins_inet/VeinsInetManagerForker.h:
+
+veins_inet/veins_inet.h:
+
+/home/veins/src/veins/src/veins/veins.h:
+
+/home/veins/src/omnetpp-5.7/include/omnetpp.h:
+
+/home/veins/src/inet/src/inet/common/INETDefs.h:
+
+/home/veins/src/inet/src/inet/common/precompiled_release.h:
+
+/home/veins/src/inet/src/inet/common/precompiled.h:
+
+/home/veins/src/inet/src/inet/opp_defines.h:
+
+/home/veins/src/inet/src/inet/features.h:
+
+/home/veins/src/inet/src/inet/common/Compat.h:
+
+/home/veins/src/inet/src/inet/common/InitStages.h:
+
+/home/veins/src/veins/src/veins/modules/mobility/traci/TraCIScenarioManagerForker.h:
+
+/home/veins/src/veins/src/veins/modules/mobility/traci/TraCIScenarioManager.h:
+
+/home/veins/src/veins/src/veins/base/utils/Coord.h:
+
+/home/veins/src/veins/src/veins/base/utils/FWMath.h:
+
+/home/veins/src/veins/src/veins/base/modules/BaseWorldUtility.h:
+
+/home/veins/src/veins/src/veins/base/connectionManager/BaseConnectionManager.h:
+
+/home/veins/src/veins/src/veins/base/utils/AntennaPosition.h:
+
+/home/veins/src/veins/src/veins/base/connectionManager/NicEntry.h:
+
+/home/veins/src/veins/src/veins/base/utils/Heading.h:
+
+/home/veins/src/veins/src/veins/modules/utility/HasLogProxy.h:
+
+/home/veins/src/veins/src/veins/base/utils/FindModule.h:
+
+/home/veins/src/veins/src/veins/modules/obstacle/ObstacleControl.h:
+
+/home/veins/src/veins/src/veins/modules/obstacle/Obstacle.h:
+
+/home/veins/src/veins/src/veins/modules/world/annotations/AnnotationManager.h:
+
+/home/veins/src/veins/src/veins/modules/utility/BBoxLookup.h:
+
+/home/veins/src/veins/src/veins/modules/obstacle/VehicleObstacleControl.h:
+
+/home/veins/src/veins/src/veins/base/utils/Move.h:
+
+/home/veins/src/veins/src/veins/modules/obstacle/MobileHostObstacle.h:
+
+/home/veins/src/veins/src/veins/modules/mobility/traci/TraCIBuffer.h:
+
+/home/veins/src/veins/src/veins/modules/mobility/traci/TraCIConstants.h:
+
+/home/veins/src/veins/src/veins/modules/mobility/traci/TraCIColor.h:
+
+/home/veins/src/veins/src/veins/modules/mobility/traci/TraCIConnection.h:
+
+/home/veins/src/veins/src/veins/modules/mobility/traci/TraCICoord.h:
+
+/home/veins/src/veins/src/veins/modules/mobility/traci/TraCICoordinateTransformation.h:
+
+/home/veins/src/veins/src/veins/modules/mobility/traci/VehicleSignal.h:
+
+/home/veins/src/veins/src/veins/base/utils/EnumBitset.h:
+
+/home/veins/src/veins/src/veins/modules/mobility/traci/TraCIRegionOfInterest.h:
+
+/home/veins/src/veins/src/veins/modules/mobility/traci/TraCILauncher.h:
+
+veins_inet/VeinsInetManagerBase.h:
+
+/home/veins/src/veins/src/veins/modules/utility/SignalManager.h:
